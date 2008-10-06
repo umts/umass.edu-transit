9907b269f6497e79cc32a4bbe0ec54440a0df3b9
thijs <thijs@802f62a4-8c4f-0410-9816-caa66071c0a2>
2008-10-06 02:38:43 +0000

    damnit
    git-svn-id: svn+ssh://dev/usr/local/svn/public@74 802f62a4-8c4f-0410-9816-caa66071c0a2

--- javascript/say.no.to.ie.6.js
+++ javascript/say.no.to.ie.6.js
@@ -49,12 +49,3 @@ if(IE6){
 	});
 }
 
-function neverGonnaGiveYouUp(){
-  var randomnumber=Math.floor(Math.random()*30);
-  if(randomnumber==4){
-    window.location = "http://www.youtube.com/watch?v=edaJP3Lp0Gg";
-  }
-  else{
-    window.location = "http://umass.edu";
-  }
-}
