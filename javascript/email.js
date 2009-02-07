ea70078e454fb6e9e538749b3ac71d0b17bbbfb5
matt <matt@802f62a4-8c4f-0410-9816-caa66071c0a2>
2009-02-07 22:35:40 +0000

    Added Andy and Bill to staff page, Fixed some ST typos etc.
    git-svn-id: svn+ssh://dev/usr/local/svn/public@120 802f62a4-8c4f-0410-9816-caa66071c0a2

--- javascript/email.js
+++ javascript/email.js
@@ -1,5 +1,5 @@
 function openwindow(name)
 {
 //name="jeff";
-window.open("https://www.umasstransit.org/email.php?sendto=" + name, "client","location=0,status=0,scrollbars=0, menubar=0, toolbar=0, directories=0,width=600,height=645");
+window.open("https://www.umasstransit.org/cams/transit/email.php?sendto=" + name, "client","location=0,status=0,scrollbars=0, menubar=0, toolbar=0, directories=0,width=600,height=645");
 }
