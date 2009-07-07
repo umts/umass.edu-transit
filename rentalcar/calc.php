4b394b6a8791c6d94edad782fcb65a8baea489ec
logan <logan@802f62a4-8c4f-0410-9816-caa66071c0a2>
2009-07-07 15:21:05 +0000

    fgjbhjknvcfyvgubh
    git-svn-id: svn+ssh://dev/usr/local/svn/public@160 802f62a4-8c4f-0410-9816-caa66071c0a2

--- rentalcar/calc.php
+++ rentalcar/calc.php
@@ -1,12 +1,5 @@
-<?
-                  
-  $mpg = 0;
-  $distance = 0;
-  $fuel_cost = 0;
-  $days_of_rental = 0;
-  $cost_of_rental_per_day = 0;
-  $reimbursement_rate = 0;
-  
-  print "FUCK FUCK FUCK";                  
-?>
-
+<html>
+  <body>
+<? println "yeah"; ?>
+  </body>
+</html>
