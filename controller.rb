270e2a5e767709a5767640545bc382ac8be36719
Matt Moretti <werebus@gmail.com>
2012-10-30 16:14:16 -0400

    Remove page_titles.yml from the compiled site

--- controller.rb
+++ controller.rb
@@ -10,7 +10,7 @@ end
 
 # Put files in here that are part of the project, but not part of the site.
 # They won't be copied into the "public" folder when stasis builds the site.
-ignore *%w{.gitignore .rvmrc capfile LICENSE Gemfile Gemfile.lock Rakefile README.md}
+ignore *%w{.gitignore .rvmrc capfile LICENSE Gemfile Gemfile.lock Rakefile README.md page_titles.yml}
 
 ignore(/\.swp$/, %r{/\.git/}, %r{/\.sass-cache/})
 ignore /\/_.*/
